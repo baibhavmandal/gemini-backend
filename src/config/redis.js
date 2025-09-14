f61fbc54aa4e349c82c6291eebc3e1b2df97a6e5
baibhavmandal <baibhavmandal1@gmail.com>
2025-09-14 18:38:49 +0000

    third update for production

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,14 +4,7 @@ import { createClient } from "redis";
 
 dotenv.config();
 
-const redisClient = createClient({
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.PORT,
-  },
-});
+const redisClient = createClient({ uri: process.env.REDIS_URI });
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
